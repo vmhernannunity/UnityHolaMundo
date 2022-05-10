07cbdb894c9543d02af338ca10cdaf344490e218
Victor HD <vmhernann@gmail.com>
2022-05-10 01:45:38 -0500

    UnityHola
    Hola mundo y mensaje a los tutores

---
+++ Work_U3P1/Assets/Scripts/HiW.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiW : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("HOLA MUNDO, me es muy dificil el uso de Git Hub y Git Kraken!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
